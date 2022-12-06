c3070956ec8e0eadbfa3f6b373500a8ce5fb6555
Emma Wise <30713299+emwise@users.noreply.github.com>
2022-12-06 09:00:12 -0600

    updated config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,12 @@
     images: {
       unoptimized: true,
     },
-  }
+  },
+  api: { 
+
+    invokeUrl: 'https://4pc6wve37j.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod, 
+
+  } 
 }
 
 module.exports = nextConfig
