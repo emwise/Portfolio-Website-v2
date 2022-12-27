5e05565b72a88327960a4a8f325b63d7905b3d59
Emma Wise <30713299+emwise@users.noreply.github.com>
2022-12-27 12:12:42 -0600

    updated out

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@
       unoptimized: true,
     },
   },
-  api: { 
+  api: {
 
     invokeUrl: 'https://4pc6wve37j.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod, 
 
